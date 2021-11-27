2ef10fc092f75ff1c7e3b88ea949c847e71a1f1f
simone <simoneaiello2000@outlook.com>
2021-11-27 18:12:19 +0100

    Initial commit

---
+++ src/main/java/com/progetto/ProgettoIngswWebApplication.java
@@ -0,0 +1,13 @@
+package com.progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoIngswWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoIngswWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progetto/ProgettoIngswWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoIngswWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
