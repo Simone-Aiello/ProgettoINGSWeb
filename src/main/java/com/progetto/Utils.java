3634da992b26774435f5352a6d5de59ac8520863
Salvatore Fiorentino <fiorentinosalvatore65@gmail.com>
2021-12-24 12:06:01 +0100

    aggiunte funzionalità cross site scripting , e introdotto concetto builder

---
+++ src/main/java/com/progetto/Utils.java
@@ -0,0 +1,23 @@
+package com.progetto;
+
+import java.util.regex.Pattern;
+
+public class Utils {
+	
+	public static final String PATTERN= "[^\\w #&;]";
+
+	
+	public static String sanitizeXSS(String string) {
+		
+		for (int i = 0; i < string.length(); i++) {
+			char character = string.charAt(i) ;
+			String c = ""+character;
+			if(Pattern.matches(PATTERN, c))
+			{
+				String newSubString = "&#"+ (int)character + ";" ;
+				string = string.replace(c,newSubString);
+			}
+		}
+		return string ;
+	}
+}
--- src/main/java/com/progetto/model/Advertise.java
+++ src/main/java/com/progetto/model/Advertise.java
@@ -1,19 +1,147 @@
 package com.progetto.model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import static org.apache.commons.lang3.Validate.*;
 import org.joda.time.DateTime;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.progetto.Utils;
+
+@RestController
 public class Advertise {
-	private long id;
+	
+	private final Long id;
 	private String description;
-	private String title;
+	private final String title;
 	private DateTime expiryDate;
-	private Account user;
-	private List<Area> interestedAreas;
-	private List<Image> images;
+	private final Account user;
+	private final List<Area> interestedAreas;
+	private final List<Image> images;
 	private Offer acceptedOffer = null;
-	private List<Offer> offers;
+	private final List<Offer> offers;
+	
+	
+	@GetMapping("/prova")
+	public Advertise.Builder getRequest(Advertise.Builder adBuilder) {
+		
+		System.out.println(adBuilder.build().title);
+		return adBuilder ;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Advertise.Builder builder = new Advertise.Builder();
+	}
+
+	public static final class Builder{
+		
+		private Advertise advertise ;
+		private boolean built = false ;
+		
+		private  Long id;
+		private  String description;
+		private  String title;
+		private  DateTime expiryDate;
+		private  Account user;
+		private  List<Area> interestedAreas;
+		private  List<Image> images;
+		private  Offer acceptedOffer = null;
+		private  List<Offer> offers;
+		
+		
+		public Builder setId(Long id) {
+			notNull(id);
+			this.id = id;
+			return this;
+		}
+		
+		public Builder setDescription(String description) {
+			notNull(description);
+			description.trim();
+			notBlank(description);
+			this.description = Utils.sanitizeXSS(description);
+			return this;
+		}
+		public Builder setTitle(String title) {
+			notNull(title);
+			title.trim();
+			notBlank(title);
+			this.title = Utils.sanitizeXSS(title);
+			return this;
+		}
+		public Builder setExpiryDate(DateTime expiryDate) {
+			notNull(expiryDate);
+			this.expiryDate = expiryDate;
+			return this;
+		}
+		public Builder setUser(Account user) {
+			notNull(user);
+			this.user = user;
+			return this;
+		}
+		public Builder setInterestedAreas(List<Area> interestedAreas) {
+			notNull(interestedAreas);
+			this.interestedAreas = interestedAreas;
+			return this;
+		}
+		public Builder setImages(List<Image> images) {
+			notNull(images);
+			this.images = images;
+			return this;
+		}
+		public Builder setAcceptedOffer(Offer acceptedOffer) {
+			notNull(acceptedOffer);
+			this.acceptedOffer = acceptedOffer;
+			return this;
+		}
+		public Builder setOffers(List<Offer> offers) {
+			notNull(offers);
+			this.offers = offers;
+			return this;
+		}
+		
+		public boolean mandatoryFieldsAreNull() {
+			
+			return id == null || description == null || title == null || 
+					expiryDate == null || interestedAreas == null || user== null ;
+		}
+		
+		
+		public Advertise build() {
+			validState(!built,"Il costruttore è stato già utilizzato, non è più valido");
+			validState(!mandatoryFieldsAreNull(),"Alcuni campi obbligatori non sono stati impostati");
+			if (!mandatoryFieldsAreNull()) 
+				built = true ;
+			advertise = new Advertise(id, description, title, expiryDate, user, interestedAreas, images,
+					acceptedOffer, offers);
+			return advertise ;
+		}
+		
+	}
+	
+	public Advertise(long id, String description, String title, DateTime expiryDate, Account user,
+			List<Area> interestedAreas, List<Image> images, Offer acceptedOffer, List<Offer> offers) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.title = title;
+		this.expiryDate = expiryDate;
+		this.user = user;
+		this.interestedAreas = interestedAreas;
+		this.images = images == null ? new ArrayList<Image>() : images;
+		this.acceptedOffer = acceptedOffer;
+		this.offers = offers == null ? new ArrayList<Offer>() : offers;
+		
+		
+	}
 	
 	public Offer getAcceptedOffer() {
 		return acceptedOffer;
@@ -24,27 +152,19 @@ public class Advertise {
 	public List<Offer> getOffers() {
 		return offers;
 	}
-	public void setOffers(List<Offer> offers) {
-		this.offers = offers;
-	}
+
 	public List<Image> getImages() {
 		return images;
 	}
-	public void setImages(List<Image> images) {
-		this.images = images;
-	}
+	
 	public List<Area> getInterestedAreas() {
 		return interestedAreas;
 	}
-	public void setInterestedAreas(List<Area> interestedAreas) {
-		this.interestedAreas = interestedAreas;
-	}
+	
 	public long getId() {
 		return id;
 	}
-	public void setId(long id) {
-		this.id = id;
-	}
+	
 	public String getDescription() {
 		return description;
 	}
@@ -54,9 +174,7 @@ public class Advertise {
 	public String getTitle() {
 		return title;
 	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
+
 	public DateTime getExpiryDate() {
 		return expiryDate;
 	}
@@ -66,9 +184,7 @@ public class Advertise {
 	public Account getUser() {
 		return user;
 	}
-	public void setUser(Account user) {
-		this.user = user;
-	}
+	
 	
 	
 }
